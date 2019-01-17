e3d412d321ff5d300b46125d731b9ef6ba00956b
GIS <GerasimenkoIS@it.mos.ru>
2019-01-17 17:20:16 +0300

    ClientsGroup.java: added name & toString()

--- src/test/java/restaurant/ClientsGroup.java
+++ src/test/java/restaurant/ClientsGroup.java
@@ -2,14 +2,20 @@ package restaurant;
 
 public class ClientsGroup {
     public final int size; // number of clients
+    public final String name;
 
 
     public ClientsGroup(int size) {
         this.size = size;
+        this.name = "";
+    }
+    public ClientsGroup(int size, String name) {
+        this.size = size;
+        this.name = name;
     }
 
     @Override
     public String toString() {
-        return "{group of size: " + size + "}";
+        return "{group of size: " + size + "; name: " + name + "}";
     }
 }
