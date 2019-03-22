37ca65ff2264b0fc31b1ce29b20b5e6ab610e350
GIS <GerasimenkoIS@it.mos.ru>
2019-03-22 12:04:31 +0300

    experiments with Excel reading / comparing

--- src/test/java/compare/excel/TestExcel.java
+++ src/test/java/compare/excel/TestExcel.java
@@ -17,16 +17,22 @@ public class TestExcel {
         Workbook actual = null;
         Workbook expected_1 = null;
         Workbook expected_2 = null;
+        Workbook actualDoubleReport = null;
+        Workbook expectedDoubleReport = null;
         try {
             actual = new HSSFWorkbook(new FileInputStream("C:\\Users\\gis\\IdeaProjects\\experi\\resources\\files\\excelToCompare\\Транспортные средства 30.01.2019.xls"));
             expected_1 = new HSSFWorkbook(new FileInputStream("C:\\Users\\gis\\IdeaProjects\\experi\\resources\\files\\excelToCompare\\Транспортные средства 30.01.2019 (1).xls"));
             expected_2 = new HSSFWorkbook(new FileInputStream("C:\\Users\\gis\\IdeaProjects\\experi\\resources\\files\\excelToCompare\\Транспортные средства 30.01.2019 (2).xls"));
+            actualDoubleReport = new HSSFWorkbook(new FileInputStream("C:\\Users\\gis\\IdeaProjects\\experi\\resources\\files\\excelToCompare\\Отчет 31.01.2019 (1).xls"));
+            expectedDoubleReport = new HSSFWorkbook(new FileInputStream("C:\\Users\\gis\\IdeaProjects\\experi\\resources\\files\\excelToCompare\\Отчет 31.01.2019 (2).xls"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         MatcherAssert.assertThat(actual, sameWorkbook(expected_1));
+        MatcherAssert.assertThat(actualDoubleReport, sameWorkbook(expectedDoubleReport));
         MatcherAssert.assertThat(actual, sameWorkbook(expected_2));
+
     }
 
 }
